454cadf3cab8fab21db6326b14c1781717728434
Birbal Kumar <birbalkr1435@gmail.com>
2023-12-23 15:23:47 +0530

    Button, View, text

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { 
+  Button,
   Text,
   View
 } from "react-native";
@@ -7,8 +8,13 @@ import {
 function App(){
   return(
     <View>
-      <Text style ={{fontSize:30}} >Hello</Text>
+      <Text style ={{fontSize:30}} >Hello Ad</Text>
+      <Text style ={{fontSize:30}} >Button 1</Text>
+      <Button title='Press Here'></Button>
+      <Text style ={{fontSize:30}} >Button 2</Text>
+      <Button title='More'></Button>
     </View>
+      
   );
 }
 
