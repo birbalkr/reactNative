c4c9daab74dc4b6aa33cc3d337bbef1887850655
Birbal Kumar <birbalkr1435@gmail.com>
2023-12-30 14:48:06 +0530

    Props and state

--- App.tsx
+++ App.tsx
@@ -5,22 +5,24 @@ import {
   View
 } from "react-native";
 
-import UserData from './components/UserData';
-
 function App() {
-  const [name, setName] = useState("Aditya");
-
-  function testname() {
-    setName("admin");
-  }
+  const [name,setName]=useState("Anil");
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>{name}</Text>
-      <Button title='update name' onPress={testname}></Button>
+      <Text style={{ fontSize: 30 }}>Props in  React js</Text>
+      <Button title='Update Props' onPress={()=>{setName("Aditya Kumar")}}/>
+      <User name={name} age={29}/>
     </View>
-
   );
 };
 
+const User=(props)=>{
+  return(
+    <View style={{backgroundColor:"green",padding:15}}>
+      <Text style={{fontSize:30}}>Name: {props.name}</Text>
+      <Text style={{fontSize:30}}>Age: {props.age}</Text>
+    </View>
+  );
+};
 
 export default App;
