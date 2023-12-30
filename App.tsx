02ab797845dc32e5e1fc37ea49bc9f61c37f367d
Birbal Kumar <birbalkr1435@gmail.com>
2023-12-30 15:30:57 +0530

    style in App

--- App.tsx
+++ App.tsx
@@ -2,27 +2,36 @@ import React, { useState } from 'react';
 import {
   Button,
   Text,
-  View
+  View,
+  StyleSheet
 } from "react-native";
-
+import ExStyle from './style'
 function App() {
-  const [name,setName]=useState("Anil");
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Props in  React js</Text>
-      <Button title='Update Props' onPress={()=>{setName("Aditya Kumar")}}/>
-      <User name={name} age={29}/>
+      <Text style={{ fontSize: 30,color:'red',backgroundColor:'lightblue' }}>Style Sheet inline</Text>
+      <Text style={style.textBox}>Style Sheet Internal</Text>
+      <Text style={ExStyle.tBox}>Style Sheet External</Text>
+      <Text style={[style.textBox,ExStyle.tBox,{marginTop:12}]}>Style Sheet two class</Text>
     </View>
-  );
+  ); 
 };
 
-const User=(props)=>{
-  return(
-    <View style={{backgroundColor:"green",padding:15}}>
-      <Text style={{fontSize:30}}>Name: {props.name}</Text>
-      <Text style={{fontSize:30}}>Age: {props.age}</Text>
-    </View>
-  );
-};
+// internal style
+const style = StyleSheet.create({
+  textBox:{
+    color:'green',
+    fontSize:20,
+    backgroundColor:'lightgreen',
+    margin:12,
+    borderRadius:25,
+    padding:12,
+    textAlign:'center',
+    borderColor:'red',
+    borderWidth:5
+  }
+})
 
+// External style
+// create a file style.js
 export default App;
---
+++ style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+export default ExStyle = StyleSheet.create({
+    tBox: {
+        color: 'green',
+        fontSize: 20,
+        backgroundColor: 'lightgreen',
+        margin: 12,
+        borderRadius: 25,
+        padding: 12,
+        textAlign: 'center',
+        borderColor: 'bule',
+        borderWidth: 5
+    }
+})
