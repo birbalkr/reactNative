6039bb001342633226211e68ece7e05cf624ebac
Birbal Kumar <birbalkr1435@gmail.com>
2023-12-30 16:33:59 +0530

    get text input value(Handling text input)

--- App.tsx
+++ App.tsx
@@ -1,37 +1,39 @@
 import React, { useState } from 'react';
 import {
   Button,
+  TextInput,
   Text,
   View,
   StyleSheet
 } from "react-native";
-import ExStyle from './style'
+  
 function App() {
+  const [name,setName]=useState('');
   return (
     <View>
-      <Text style={{ fontSize: 30,color:'red',backgroundColor:'lightblue' }}>Style Sheet inline</Text>
-      <Text style={style.textBox}>Style Sheet Internal</Text>
-      <Text style={ExStyle.tBox}>Style Sheet External</Text>
-      <Text style={[style.textBox,ExStyle.tBox,{marginTop:12}]}>Style Sheet two class</Text>
+      <Text style={{fontSize:30}}>Handle Text Input</Text>
+      <Text style={{fontSize:30}}>Your Name is: {name} </Text>
+      <TextInput style={style.TextInput}
+      placeholder='Enter Your Name'
+      value={name}
+      onChangeText={(text)=>setName(text)}
+      />
+      <Button title='Clear Input Box' onPress={()=>setName('')}></Button>
     </View>
   ); 
 };
 
 // internal style
 const style = StyleSheet.create({
-  textBox:{
+  TextInput:{
     color:'green',
     fontSize:20,
     backgroundColor:'lightgreen',
     margin:12,
     borderRadius:25,
     padding:12,
-    textAlign:'center',
     borderColor:'red',
     borderWidth:5
   }
-})
-
-// External style
-// create a file style.js
+});
 export default App;
