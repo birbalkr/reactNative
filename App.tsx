7a4d8811e05671b0d91fcf376afc64ae1e697b57
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-02 17:26:18 +0530

    UseEffect

--- App.tsx
+++ App.tsx
@@ -1,38 +1,49 @@
-import React, { Component} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Text,
   View,
   FlatList,
-  SectionList,
-  Button,
-  TextInput
+  Button
 } from "react-native";
-import Student from './components/Student';
 
-class App extends Component{
-  constructor(){
-    super();
-    this.state={
-      name:"Aditya"
-    }
-  }
-  updateName(text){
-    this.setState({name:text})
-  }
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(100);
 
-  render(){
-    return(
-      <View >
-        <Text style={{fontSize:30,color:'red'}} >{this.state.name}</Text>
-        <TextInput placeholder='Enter Your Name'
-        onChangeText={(text)=>this.updateName(text)}/>
-        <Button title='Press'></Button>
-        <Student name={this.state.name} />
-      </View>
-    )
-  }
+  // useEffect(()=>{
+  //   console.warn("hello");
 
-}
+  // },[count])
+  // useEffect(()=>{
+  //   console.warn("call some api");
 
+  // },[data])
+  return (
+    <View>
+      <Text style={{ fontSize: 25 }}>{data} UseEffect {count}</Text>
+      <Button title='UpdateCount' onPress={() => setCount(count + 1)} />
+      <Button title='UpdateCount' onPress={() => setData(data + 1)} />
+      <User info={{ data, count }} />
+    </View>
+  );
+};
+
+const User = (props) => {
+
+  useEffect(() => {
+    console.warn("run this code when data prop is updated");
+  }, [props.info.data])
+
+  useEffect(() => {
+    console.warn("run this code when count prop is updated");
+  }, [props.info.count])
+
+  return (
+    <View>
+      <Text style={{ fontSize: 25, color: 'orange' }}>data: {props.info.data}</Text>
+      <Text style={{ fontSize: 25, color: 'orange' }}>count: {props.info.count}</Text>
+    </View>
+  )
+};
 
 export default App;
