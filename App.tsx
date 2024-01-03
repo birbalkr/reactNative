14d0850cff79d72ba2a6674095cb6d87711506e9
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-03 15:10:19 +0530

    Radio Button

--- App.tsx
+++ App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
     Text,
     TouchableHighlight,
+    TouchableOpacity,
     View
 } from "react-native";
 
 const App = () => {
+    const [radio, setRadio]=useState(1)
     return (
             
             <View style={style.main}>
-                <Text style={{ fontSize: 25, color: 'red' }}>TouchableHighlight Button</Text>
-                <TouchableHighlight>
-                    <Text style={[style.button,style.Buttontext]}>Button</Text>
-                </TouchableHighlight>
-                <TouchableHighlight>
-                    <Text style={[style.Success, style.Buttontext]}>Success</Text>
-                </TouchableHighlight>
-                <TouchableHighlight>
-                    <Text style={[style.Error, style.Buttontext]}>Warning</Text>
-                </TouchableHighlight>
+                
+                <Text style={{ fontSize: 25, color: 'red' }}>Radio Button</Text>
+                <TouchableOpacity onPress={()=>setRadio(1)}>
+                    <View style={style.radiorap}>
+                        <View style={style.radio}>
+                            {
+                                radio ===1 ?  <View style={style.radiobg}></View> :null
+                            }
+                        </View>
+                        <Text>Radio 1</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>setRadio(2)}>
+                    <View style={style.radiorap}>
+                        <View style={style.radio}>
+                        {
+                                radio ===2 ?  <View style={style.radiobg}></View> :null
+                        }
+                        </View>
+                        <Text>Radio 1</Text> 
+                    </View>
+                </TouchableOpacity>
             </View>
         
     )
@@ -27,45 +41,30 @@ const App = () => {
 
 const style=StyleSheet.create({
     main:{
-        flex:1
-    },
-    button:{
-        backgroundColor:'#bbb',
-        fontSize:30,
-        textAlign:'center',
-        padding:10,
+        flex:1,
         margin:10,
-        borderRadius:10,
-        shadowColor:'black',
-        elevation:10,
-        shadowOpacity:1
+        alignItems:'center',
+        justifyContent:'center'
     },
-    Success:{
-        backgroundColor:'green',
-        fontSize:30,
-        textAlign:'center',
-        padding:10,
-        margin:10,
-        borderRadius:10,
-        shadowColor:'black',
-        elevation:10,
-        shadowOpacity:1
+    radio:{
+        height:40,
+        width:40,
+        borderColor:'black',
+        borderRadius:20,
+        borderWidth:2,
+        margin:10
     },
-    Error:{
-        backgroundColor:'lightblue',
-        fontSize:30,
-        textAlign:'center',
-        padding:10,
-        margin:10,
-        borderRadius:10,
-        shadowColor:'black',
-        elevation:10,
-        shadowOpacity:1
+    radiorap:{
+        flexDirection:'row',
+        alignItems:'center'
     },
-    Buttontext:{
-        color:'#fff'
+    radiobg:{
+        backgroundColor:'black',
+        height:28,
+        width:28,
+        borderRadius:20,
+        margin:4
     }
-
 })
 
 export default App;
