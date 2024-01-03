45d9aabc9dda5bb9126d48899534d7559254a605
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-03 14:20:04 +0530

    Responsive UI With flex

--- App.tsx
+++ App.tsx
@@ -1,36 +1,63 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
+    StyleSheet,
     Text,
-    View,
-    FlatList,
-    Button
+    View
 } from "react-native";
-const App = () => {
-    const [show, setShow] = useState(true)
-    return (
-        <View style={{ padding: 12 }}>
-            <Text style={{ fontSize: 25, color: 'red' }}>useEffect for Unmount</Text>
-            <Button title='Toggle' onPress={()=>setShow(!show)} />
-            {
-            show ? <Student /> :null
-            }
-        </View>
-    )
-}
-const Student = () => {
-    useEffect(()=>{
-        return()=>{
-            console.warn("useEffect called on unmount");
-        }
-        
-    })
-    return (
-        <View>
-            <Text style={{ fontSize: 20, color: 'green' }}>Student Component</Text>
-        </View>
-    )
-}
 
+const App = () => {
+    return (
+        <View style={style.main}>
+            <Text style={{ fontSize: 25, color: 'red' }}>Responsive UI With flex</Text>
+            <View style={style.box1}>
+                <View style={style.innerbox}></View>
+                <View style={style.innerbox1}></View>
+                <View style={style.innerbox2}></View>
+
+            </View>
+            <View style={style.box2}></View>
+            <View style={style.box3}></View>
+        </View>
+    )
+};
+
+const style=StyleSheet.create({
+    main:{
+        flex:1
+    },
+    box1:{
+        flex: 1, 
+        backgroundColor:'red',
+        flexDirection:'row'
+    }
+    ,
+    box2:{
+        flex: 1, 
+        backgroundColor:'green'
+    }
+    ,
+    box3:{
+        flex: 1, 
+        backgroundColor:'blue'
+    },
+    innerbox:{
+        flex:1,
+        backgroundColor:'skyblue',
+        margin:10
+    }
+    ,
+    innerbox1:{
+        flex:1,
+        backgroundColor:'yellow',
+        margin:10
+    }
+    ,
+    innerbox2:{
+        flex:1,
+        backgroundColor:'orange',
+        margin:10
+    }
+})
 export default App;
 
 // npx react-native start
