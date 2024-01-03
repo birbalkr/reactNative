f2689425ab4b17c085a3e9fa42b44cde32cff8c2
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-03 15:44:29 +0530

    Activity Indicator

--- App.tsx
+++ App.tsx
@@ -1,59 +1,30 @@
 import React, { useState } from 'react';
 import {
+    ActivityIndicator,
+    Button,
     StyleSheet,
     Text,
-    TouchableHighlight,
-    TouchableOpacity,
     View
 } from "react-native";
 
 const App = () => {
-    const skill=[
-        {
-            id:1,
-            name:'Java'
-        },
-        {
-            id:2,
-            name:'C++'
-        },
-        {
-            id:3,
-            name:'React'
-        },
-        {
-            id:4,
-            name:'JavaScript'
-        },
-        {
-            id:5,
-            name:'PHP'
-        },
-        {
-            id:6,
-            name:'Python'
-        },
-    ]
-    const [radio, setRadio]=useState(1)
+    const [show ,setShow]=useState(false)
+    const displayLoader=()=>{
+        setShow(true);
+
+        setTimeout(() => {
+            setShow(false)
+        }, 3000);
+
+        // api call in 3s
+    }
     return (
-            
             <View style={style.main}>
-                
-                <Text style={{ fontSize: 25, color: 'red' }}>Dynamic Radio Button</Text>
-                {
-                    skill.map((item,index)=> <TouchableOpacity 
-                    key={index}
-                    onPress={()=>setRadio(item.id)}>
-                    <View style={style.radiorap}>
-                        <View style={style.radio}>
-                            {
-                                radio ===item.id ?  <View style={style.radiobg}></View> :null
-                            }
-                        </View>
-                        <Text>{item.name}</Text>
-                    </View>
-                </TouchableOpacity>)
-                }
+                <Text style={{ fontSize: 25, color: 'red' }}>Activity Indicator</Text>
+                {/* <ActivityIndicator size={"large"} color={'red'} animating={true}/>
+                <ActivityIndicator size={200} color={'blue'} animating={false}/> */}
+                <ActivityIndicator size={100} color={'green'} animating={show}/>
+                <Button title='show loder' onPress={displayLoader}/>
             </View>
         
     )
@@ -66,25 +37,6 @@ const style=StyleSheet.create({
         alignItems:'center',
         justifyContent:'center'
     },
-    radio:{
-        height:40,
-        width:40,
-        borderColor:'black',
-        borderRadius:20,
-        borderWidth:2,
-        margin:10
-    },
-    radiorap:{
-        flexDirection:'row',
-        alignItems:'center'
-    },
-    radiobg:{
-        backgroundColor:'black',
-        height:28,
-        width:28,
-        borderRadius:20,
-        margin:4
-    }
 })
 
 export default App;
