e62c525a9b78efc84bea9096fe54fc1351117508
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-04 15:20:33 +0530

    WebView install

--- App.tsx
+++ App.tsx
@@ -1,47 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
     Button,
-    Pressable,
-    StatusBar,
+    Platform,
+    ScrollView,
     StyleSheet,
     Text,
     View
 } from "react-native";
+import WebView from 'react-native-webview';
 
 const App = () => {
-    const [hide, setHide]=useState(false)
-    const [barstyle,setBarStyle]=useState('defult')
-    return (
-        <View style={style.main}>
-            <Text style={{ fontSize: 25, color: 'red' }}>Status Bar</Text>
-            <StatusBar 
-            backgroundColor="red"
-            barStyle={barstyle}
-            hidden={hide}   
-            />
-            <Button title='Toggle Status bar' onPress={()=>setHide(!hide)}></Button>
-            <Button title='Update Style' onPress={()=>setBarStyle('dark-content')}></Button>
-        </View>
 
+    return (
+        <WebView source={{uri:"https://www.hackthebox.com/"}}/>
     )
 };
 
 const style = StyleSheet.create({
-    main: {
-        flex: 1,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    buttonview: {
-        backgroundColor:'blue',
-        color:'#fff',padding:10,
-        margin:10,
-        borderRadius:10,
-        fontSize:20,
-        textAlign:'center',
-        shadowColor:"red",
-        elevation:20
-    },
+    
 })
 
 export default App;
