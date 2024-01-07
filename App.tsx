bf18053a6dea76255612ecf7ebee949734103595
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-08 05:29:32 +0530

    stack navigation style

--- App.tsx
+++ App.tsx
@@ -3,18 +3,20 @@ import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen(props) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
+            <Button title='About Screen' onPress={()=>props.navigation.navigate('About')}/>
         </View>
     );
 }
 const Login=(props)=> {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>Login Screen</Text>
             <Button title='Home Screen' onPress={()=>props.navigation.navigate('Home')}/>
+            <Button title='About Screen' onPress={()=>props.navigation.navigate('About')}/>
         </View>
     );
 }
@@ -24,14 +26,39 @@ const Stack = createNativeStackNavigator();
 function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Login" component={Login} />
+            <Stack.Navigator 
+            screenOptions={{
+            headerStyle:{
+                backgroundColor:'orange'
+            },
+            headerTitleStyle:{
+                fontSize:25
+            },
+            headerTintColor:'white'}}>
+                <Stack.Screen name="Login" component={Login}  />
                 <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="About" component={About}   
+                options={{
+            headerStyle:{
+                backgroundColor:'white'
+            },
+            headerTitleStyle:{
+                fontSize:25
+            },
+            headerTintColor:'orange'}}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
 
+const About=(props)=> {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>About Screen</Text>
+            <Button title='Home Screen' onPress={()=>props.navigation.navigate('Home')}/>
+        </View>
+    );
+}
 
 
 export default App;
