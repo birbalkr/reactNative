27e1445c510219143ecfd0ca37dd9ae647c689c7
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-08 06:11:48 +0530

    Passing data one screen to other screen.

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 function HomeScreen(props) {
+    console.warn(props.route.params);
+    const {name, age}=props.route.params;
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>Passing Data two way</Text>
+            <Text>Name: {props.route.params.name}</Text>
+            <Text>Age: {props.route.params.age}</Text>
+            <Text>Name: {name} Age {age}</Text>
             <Button title='About Screen' onPress={() => props.navigation.navigate('About')} />
         </View>
     );
--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 const Login = (props) => {
+    // const name='aditya';
+    const age=29;
+    const [name,setName]=React.useState("");
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Login Screen</Text>
-            <Button title='Home Screen' onPress={() => props.navigation.navigate('Home')} />
+            <Text>Login Screen Passing Data to home Screen</Text>
+            <TextInput placeholder='Enter name' onChangeText={(text)=>setName(text)}/>
+            <Button title='go to Home Screen' onPress={() => props.navigation.navigate('Home',{name,age})} />
             <Button title='About Screen' onPress={() => props.navigation.navigate('About')} />
         </View>
     );
