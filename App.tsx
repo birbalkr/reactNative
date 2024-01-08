16faa685947e0e6f6b4db8dae4766c896226ac91
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-09 03:48:02 +0530

    Call own API

--- App.tsx
+++ App.tsx
@@ -3,27 +3,28 @@ import { View, Text, Button, ScrollView, FlatList } from 'react-native';
 
 
 function App() {
-    const [data, setData] = useState([]);
-    const getAPIData = async () => {
+    const saveApiData = async () => {
+        const data = {
+            name: "sam",
+            age: 34,
+            email: "asb@gmail.com"
+        }
         const url = "http://192.168.43.106:3000/users";
-        let result = await fetch(url);
-        result = await result.json();
-        setData(result)
+        let result = await fetch(url,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            })
+            result =await result.json();
+            console.warn(result);
+            
     }
-    useEffect(() => {
-        getAPIData();
-    }, [])
+
     return (
         <View >
-            <Text style={{ fontSize: 20 }}>Call json-server API</Text>
-            {
-                data.length?
-                data.map((item)=><View style={{borderColor:'red', borderWidth:1, margin:12, padding:12}}>
-                    <Text style={{ fontSize: 20 }}>name: {item.name}</Text>
-                    <Text style={{ fontSize: 20 }}>id: {item.id}</Text>
-                    <Text style={{ fontSize: 20 }}>email: {item.email}</Text>
-                </View>):null
-            }
+            <Text style={{ fontSize: 20 }}>Call own API</Text>
+            <Button title='save data' onPress={saveApiData}/>
         </View>
     )
 };
