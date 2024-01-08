3473a6e3e5c8c4d3599293446d181b82a0d579d2
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-08 06:22:39 +0530

    start tab

--- App.tsx
+++ App.tsx
@@ -1,56 +1,36 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './components/Login';
-import HomeScreen from './components/Home';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
-    const btn=()=>{
-        console.warn('Button Press');
-        
-    }
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerStyle: {
-                        backgroundColor: 'orange'
-                    },
-                    headerTitleStyle: {
-                        fontSize: 25
-                    },
-                    headerTintColor: 'white'
-                }}>
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="About" component={About}
-                    options={{
-                        headerTitle:(props)=><Button title='left' onPress={btn}/>,
-                        headerStyle: {
-                            backgroundColor: 'white'
-                        },
-                        headerTitleStyle: {
-                            fontSize: 25
-                        },
-                        headerTintColor: 'orange'
-                    }} />
-            </Stack.Navigator>
+            <Tab.Navigator>
+                <Tab.Screen name='Login' component={Login} />
+                <Tab.Screen name='Signup' component={SignUp} />
+            </Tab.Navigator>
         </NavigationContainer>
-    );
+    )
+};
+const Login = () => {
+    return (
+        <View>
+            <Text>Login</Text>
+        </View>
+    )
+}
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+        </View>
+    )
 }
 
-const About = (props) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>About Screen</Text>
-            <Button title='Home Screen' onPress={() => props.navigation.navigate('Home')} />
-        </View>
-    );
-}
 
 
 export default App;
