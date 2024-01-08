8e9dad82dc1e8822b10fea471f923d52122082ea
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-08 06:29:55 +0530

    Top Tab Navigator

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 function App() {
     return (
